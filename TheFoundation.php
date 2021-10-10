a95dab154b9167fa56b1916d7eb7bbae0920cf84
Gino D <gdambra@facepoint.co>
2021-10-10 18:38:16 +0200

    Rename main folder

--- TheFoundation.php
+++ TheFoundation.php
@@ -9,7 +9,7 @@ define('APPLICATION_PATH', __DIR__ . '/../');
 *
 */
 spl_autoload_register(function(string $classname) {
-    if (file_exists($filename = APPLICATION_PATH . '/packages/' . str_replace('\\', '/', $classname) . '.php'))
+    if (file_exists($filename = __DIR__ . '/../packages/' . str_replace('\\', '/', $classname) . '.php'))
         return require_once $filename;
 
     return false;
@@ -19,13 +19,13 @@ spl_autoload_register(function(string $classname) {
  * 
  */
 foreach([
-	__DIR__ . '/TheFoundation/RouterHttp.php',
-	__DIR__ . '/TheFoundation/RouterHttp/Response.php',
-	__DIR__ . '/TheFoundation/RouterHttp/Response/Template.php',
-	__DIR__ . '/TheFoundation/Request.php',
-	__DIR__ . '/TheFoundation/PDOFactory.php',
-	__DIR__ . '/TheFoundation/Database/Entity.php',
-	__DIR__ . '/TheFoundation/Database/Dashboard.php',
+	__DIR__ . '/src/RouterHttp.php',
+	__DIR__ . '/src/RouterHttp/Response.php',
+	__DIR__ . '/src/RouterHttp/Response/Template.php',
+	__DIR__ . '/src/Request.php',
+	__DIR__ . '/src/PDOFactory.php',
+	__DIR__ . '/src/Database/Entity.php',
+	__DIR__ . '/src/Database/Dashboard.php',
 ] as $classname)
 	require_once $classname;
 ?>
